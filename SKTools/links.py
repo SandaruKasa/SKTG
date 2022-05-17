9ac27f570d9506468b19616693d55b2a6a06f057
SandaruKasa <sandarukasa@ya.ru>
2022-05-18 01:36:23 +0300

    Update links.py
    Again

--- SKTools/links.py
+++ SKTools/links.py
@@ -3,6 +3,6 @@ from typing import Final
 
 telegram_bot_api: Final = 'https://api.telegram.org/bot{}/'
 vk_api: Final = 'https://api.vk.com/method/'
-github_repository_link: Final = 'https://github.com/SandaruKasa/SKTG/tree/python'
+github_repository_link: Final = 'https://github.com/SandaruKasa/SKTG/tree/python-old'
 booru_api: Final = 'https://{}/index.php'
 inspirobot_api: Final = 'https://www.inspirobot.me/api?generate=true'
