498bcecbbc5672aba7f46d3e053b7df67fa3e708
SandaruKasa <sandarukasa@ya.ru>
2022-06-22 21:04:37 +0300

    Configuration tweaks

--- startup.sh
+++ startup.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
+set -euxo pipefail
 cd "$(dirname "$0")"
-#/bin/bash requirements.sh
+
+if ! which ffmpeg > /dev/null; then
+	sudo apt install ffmpeg
+fi
+
+if [ ! -d venv ]; then
+    python3.10 -m venv venv
+fi
+source venv/bin/activate
+pip install -r requirements.txt
+
 python3 BetaLupi.py > betalupi/logs/$( date +%s).log& 2> betalupi/logs/error_$( date +%s).log
 python3 ImpDro.py > impdro/logs/$( date +%s).log& 2> impdro/logs/error_$( date +%s).log
-python3 Inspirobot.py > inspirobot/logs/$( date +%s).log& 2> inspirobot/logs/error_$( date +%s).log
+#python3 Inspirobot.py > inspirobot/logs/$( date +%s).log& 2> inspirobot/logs/error_$( date +%s).log
