6223c43b1bef9bf5bb23b57a146f045b4e8003ee
SandaruKasa <sandarukasa@ya.ru>
2022-08-12 21:09:06 +0300

    Rerun self as module

--- sktg/__main__.py
+++ sktg/__main__.py
@@ -1,6 +1,19 @@
 """A file that starts the bots.
 Created to allow running the bots with ``python3 -m sktg``.
 """
+if not __package__:  # a dirty hack to ensure this is run as a module
+    import os
+    import sys
+    from pathlib import Path
+
+    module_dir = Path(__file__).parent.resolve()
+    module_name = module_dir.name
+    expected_pwd = module_dir.parent
+
+    os.chdir(expected_pwd)
+    return_code = os.system(f"{sys.executable} -m {module_name}")
+    exit(return_code)
+
 import logging
 import os
 from datetime import datetime
